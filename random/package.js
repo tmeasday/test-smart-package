41b7722ce3ddb441be22b23f11c8a30936051dc4
Tom Coleman <tom@thesnail.org>
2012-07-01 13:12:26 +1000

    Small fix plus added a file

--- random/package.js
+++ random/package.js
@@ -1,3 +1,8 @@
 Package.describe({
-  summary: "A package that does nothing";
+  summary: "A package that does nothing"
+});
+
+
+Package.on_use(function (api) {
+  api.add_files('test-smart-package.js', 'client');
 });
